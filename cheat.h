715c1317e6e38eee3f4af1aa54b2deacfad1c9ff
Meastkill <166447667+Meastkill@users.noreply.github.com>
2025-12-26 00:41:18 +0100

    Cleaned up project. Made it clearer

---
+++ cheat.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <Windows.h>
+#include <iostream>
+
+class cheat
+{
+public:
+	static int hook();
+	static void main();
+};
--- main.cpp
+++ main.cpp
@@ -3,41 +3,10 @@
 #include <TlHelp32.h>
 
 #include "proc.h"
+#include "cheat.h"
 
-bool healthBool = false;
-bool ammoBool = false;
-
-void main() {
-	DWORD pID, baseModule, localPlayerPtr;
-
-	int health = 6969;
-	int ammo = 1337;
-
-	pID = GetProcessID(L"ac_client.exe");
-
-	baseModule = GetModuleBaseAddress(pID, L"ac_client.exe");
-
-	if (pID != 0 && baseModule != 0)
-	{
-		std::cout << "ProcessID: " << std::hex << pID << std::endl;
-		std::cout << "BaseModuleAddress: " << std::hex << baseModule << std::endl;
-	}
-	else
-	{
-		std::cout << "Assault Cube was not found. Please Open Assault Cube \n" << std::endl;
-		system("pause");
-		exit(EXIT_FAILURE);
-	}
-
-	HANDLE handle = OpenProcess(PROCESS_ALL_ACCESS, NULL, pID);
-
-	ReadProcessMemory(handle, (LPCVOID)(baseModule + 0x17E0A8), &localPlayerPtr, sizeof(localPlayerPtr), nullptr);
-
-	while (true)
-	{
-		WriteProcessMemory(handle, (LPVOID)(localPlayerPtr + 0xEC), &health, sizeof(health), nullptr); // health overwrite
-
-		WriteProcessMemory(handle, (LPVOID)(localPlayerPtr + 0x140), &ammo, sizeof(ammo), nullptr); // rifle ammo overwrite
-		WriteProcessMemory(handle, (LPVOID)(localPlayerPtr + 0x12C), &ammo, sizeof(ammo), nullptr); // pistol ammo overwrite
-	}
+void main() 
+{
+	cheat::hook();
+	cheat::main();
 }
